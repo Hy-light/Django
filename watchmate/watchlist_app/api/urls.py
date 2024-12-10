64d4ae09725b7ffb7b09ee4d2620d5514b6c4e1d
Prince Chime <chimeprince92@gmail.com>
2024-12-10 16:00:06 +0000

    Modified project to use Class Based View

--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
-from watchlist_app.api.views import movie_list, movie_detail
+from watchlist_app.api.views import MovieListAV, MovieDetailAV
 
 urlpatterns = [
     # Add your URL patterns here.
-    path('list/', movie_list, name='movie-list' ),
-    path('<int:pk>', movie_detail, name='movie-detail' ),  # Add this line to create a detail view for each movie.
+    path('list/', MovieListAV.as_view(), name='movie-list' ),
+    path('<int:pk>', MovieDetailAV.as_view(), name='movie-detail' ),  # Add this line to create a detail view for each movie.
     
 ]
