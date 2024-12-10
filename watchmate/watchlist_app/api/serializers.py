ba1ad4d16402c6b1ea2b5566506c9a3e1243c2e8
Prince Chime <chimeprince92@gmail.com>
2024-12-10 17:15:30 +0000

    explored working with model serializers

--- watchmate/watchlist_app/api/serializers.py
+++ watchmate/watchlist_app/api/serializers.py
@@ -1,37 +1,21 @@
 from rest_framework import serializers
 from watchlist_app.models import Movie
 
-def name_length(value):
-    if len(value) < 5:
-        raise serializers.ValidationError('Name should be at least 5 characters long.')
-
-
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(validators=[name_length])
-    description = serializers.CharField()
-    active = serializers.BooleanField()
+# Model.Serializers
+class MovieSerializer(serializers.ModelSerializer):
     
+    class Meta:
+        model = Movie
+        fields = "__all__"         # allows for all fields to be serialized or deserialized
+        # fields = ('name', 'description', 'active')  # allows for only specific fields to be serialized or deserialized.
+        # exclude = ('id',)  # excludes a desired field from the serialized output.
     
-    def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
-    
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.description = validated_data.get('description', instance.description)
-        instance.active = validated_data.get('active', instance.active)
-        instance.save()
-        return instance
-    
-    # object level validation
     def validate(self, data):
         if data['name'] == data['description']:
             raise serializers.ValidationError('Title and description should not be the same.')
         return data
     
-    # field level validation
-    # def validate_name(self, value):
-    #     if len(value) < 5:
-    #         raise serializers.ValidationError('Name should be at least 5 characters long.')
-    #     return value
-    
+    def validate_name(self, value):
+        if len(value) < 5:
+            raise serializers.ValidationError('Name should be at least 5 characters long.')
+        return value
