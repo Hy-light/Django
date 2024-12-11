8b29fb66783a564bb144f4ab2ed4d8311aba11f0
Prince Chime <chimeprince92@gmail.com>
2024-12-11 10:32:50 +0000

    used custom serializer fields

--- watchmate/watchlist_app/api/serializers.py
+++ watchmate/watchlist_app/api/serializers.py
@@ -3,12 +3,23 @@ from watchlist_app.models import Movie
 
 # Model.Serializers
 class MovieSerializer(serializers.ModelSerializer):
+    # custom serializer fields.
+    len_names = serializers.SerializerMethodField()
+    len_desc = serializers.SerializerMethodField()
     
     class Meta:
         model = Movie
         fields = "__all__"         # allows for all fields to be serialized or deserialized
         # fields = ('name', 'description', 'active')  # allows for only specific fields to be serialized or deserialized.
         # exclude = ('id',)  # excludes a desired field from the serialized output.
+    
+    # custom methods for calculating the length of the movie description
+    def get_len_desc(self, obj):
+        return len(obj.description)
+    
+    # calculate 
+    def get_len_names(self, obj):
+        return len(obj.name)
     
     def validate(self, data):
         if data['name'] == data['description']:
@@ -19,3 +30,4 @@ class MovieSerializer(serializers.ModelSerializer):
         if len(value) < 5:
             raise serializers.ValidationError('Name should be at least 5 characters long.')
         return value
+  
